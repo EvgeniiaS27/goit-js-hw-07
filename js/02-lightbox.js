1d60c048449a2e100edc668357a99945c5245bf0
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-05-27 22:48:59 +0300

    Task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const makeImage = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`;
+};
+
+const makeGalleryImage = galleryItems.map(makeImage).join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", makeGalleryImage);
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+}
+
+const galleryLightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionPosition: "botton",
+  captionDelay: 250,
+});
